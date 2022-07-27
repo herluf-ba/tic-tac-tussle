4cf0923fb35f5a6362d39302f700fc427faaafbc
HerlufB <herlufbaggesen13@gmail.com>
2022-07-27 13:00:37 +0200

    Update lib.rs

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -1,11 +1,13 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+/// Struct for storing player related data. In tic-tac-toe the only thing we need is the name.
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Player {
     pub name: String,
 }
 
+/// Possible GameStates for a tile in the board
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum Tile {
     Empty,
@@ -13,26 +15,31 @@ pub enum Tile {
     Tac,
 }
 
+/// The different stages a game can be in
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
-pub enum State {
+pub enum Stage {
     PreGame,
     Playing,
     Ended,
 }
 
+// This just makes it easier to dissern between a player id and any ol' u64
+type PlayerId = u64;
+
+/// A GameState object that is able to keep track of a game of TicTacTussle
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct GameState {
-    pub state: State,
+    pub stage: Stage,
     pub board: [Tile; 9],
-    pub active_player_id: u64,
-    pub players: HashMap<u64, Player>,
+    pub active_player_id: PlayerId,
+    pub players: HashMap<PlayerId, Player>,
     pub history: Vec<GameEvent>,
 }
 
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            state: State::PreGame,
+            GameState: GameState::PreGame,
             board: [
                 Tile::Empty,
                 Tile::Empty,
@@ -51,85 +58,52 @@ impl Default for GameState {
     }
 }
 
-/// The various reasons why game has been ended
+/// The various reasons why a game could end
 #[derive(Debug, Clone, Serialize, PartialEq, Deserialize)]
 pub enum EndGameReason {
     // In tic tac toe it doesn't make sense to keep playing when one of the players disconnect.
     // Note that it might make sense to keep playing in some other game (like Team Fight Tactics for instance).
-    PlayerDisconnected { player_id: u64 },
-    PlayerWon { winner: u64 },
+    PlayerLeft { player_id: PlayerId },
+    PlayerWon { winner: PlayerId },
 }
 
+/// An event that progresses the GameGameState forward
 #[derive(Debug, Clone, Serialize, PartialEq, Deserialize)]
 pub enum GameEvent {
     BeginGame {
-        goes_first: u64,
+        goes_first: PlayerId,
     },
     EndGame {
         reason: EndGameReason,
     },
     PlayerJoined {
-        player_id: u64,
+        player_id: PlayerId,
         name: String,
     },
     PlayerDisconnected {
-        player_id: u64,
+        player_id: PlayerId,
     },
     PlaceTile {
-        player_id: u64,
+        player_id: PlayerId,
         tile: Tile,
         at: usize,
     },
 }
 
 impl GameState {
-    pub fn consume(&mut self, event: &GameEvent) {
-        use GameEvent::*;
-        match event {
-            BeginGame { goes_first } => self.active_player_id = *goes_first,
-            EndGame { reason: _ } => self.state = State::Ended,
-            PlayerJoined { player_id, name } => {
-                self.players.insert(
-                    *player_id,
-                    Player {
-                        name: name.to_string(),
-                    },
-                );
-            }
-            PlayerDisconnected { player_id } => {
-                self.players.remove(player_id);
-            }
-            PlaceTile {
-                player_id,
-                tile,
-                at,
-            } => {
-                self.board[*at] = *tile;
-                self.active_player_id = self
-                    .players
-                    .keys()
-                    .find(|id| *id != player_id)
-                    .unwrap()
-                    .clone();
-            }
-        }
-
-        self.history.push(event.clone());
-    }
-
+     /// Determines whether an event is valid considering the current GameState
     pub fn validate(&self, event: &GameEvent) -> bool {
         use GameEvent::*;
-
         match event {
             BeginGame { goes_first } => {
                 let player_is_unknown = self.players.contains_key(goes_first);
-                if self.state != State::PreGame || player_is_unknown {
+                if self.stage != Stage::PreGame || player_is_unknown {
                     return false;
                 }
             }
             EndGame { reason } => match reason {
                 EndGameReason::PlayerWon { winner: _ } => {
-                    if self.state != State::Playing {
+                    if self.stage != Stage::Playing {
                         return false;
                     }
                 }
@@ -166,5 +140,41 @@ impl GameState {
         }
 
         true
+    }
+    
+    /// Consumes an event, modifying the GameState and adding the event to its history
+    /// NOTE: consume assumes the event to have already been validated and will accept *any* event passed to it 
+    pub fn consume(&mut self, valid_event: &GameEvent) {
+        use GameEvent::*;
+        match valid_event {
+            BeginGame { goes_first } => self.active_player_id = *goes_first,
+            EndGame { reason: _ } => self.stage = Stage::Ended,
+            PlayerJoined { player_id, name } => {
+                self.players.insert(
+                    *player_id,
+                    Player {
+                        name: name.to_string(),
+                    },
+                );
+            }
+            PlayerDisconnected { player_id } => {
+                self.players.remove(player_id);
+            }
+            PlaceTile {
+                player_id,
+                tile,
+                at,
+            } => {
+                self.board[*at] = *tile;
+                self.active_player_id = self
+                    .players
+                    .keys()
+                    .find(|id| *id != player_id)
+                    .unwrap()
+                    .clone();
+            }
+        }
+
+        self.history.push(valid_event.clone());
     }
 }
